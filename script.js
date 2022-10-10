53a819c9ba48be404e14e704f852c219fb3daa0c
Matheus Farias <math.systems.dev@gmail.com>
2022-10-10 15:24:43 -0300

    Todos os exercícios resolvidos no script.js

--- script.js
+++ script.js
@@ -25,8 +25,75 @@ var grauCelsius = ((temperaturaFH - 32) * 5) / 9;
 
 alert('O valor '+ temperaturaFH + ' em Celsius é: '+ grauCelsius);
 
+// --------------------------------------------------------------------
+
 //exercício 04
+var comprimento = parseInt(prompt('Digite o comprimento: ')),
+largura = parseInt(prompt('Digite a largura: ')), 
+altura = parseInt(prompt('Digite a altura'));
+
+var volume = comprimento * largura * altura;
+
+alert('A área da caixa é: ' + volume );
+
+// --------------------------------------------------------------------
+
+//exercício 05
+var numeroInteiro = parseInt(prompt('Digite um numero inteiro: '));
+
+var aoQuadrado = Math.pow(numeroInteiro, 2);
+
+alert('O numero: ' + numeroInteiro + ' ao quadrado é: '+ aoQuadrado);
+
+// --------------------------------------------------------------------
+
+//exercício 06
+var z = parseInt(prompt('Digite o 1° número: '));
+var x = parseInt(prompt('Digite o 2° número: '));
+var c = parseInt(prompt('Digite o 3° número: '));
+var a = parseInt(prompt('Digite o 4° número: '));
+var b = parseInt(prompt('Digite o 5° número: '));
+
+var soma = a + b + c + z + x;
+var media = (a + b + c + z + x) / 5;
+
+alert('A soma dos valores informados é: ' + soma + ' e a média das notas é: ' + media);
+
+// --------------------------------------------------------------------
+
+//exercício 07
+var n1 = parseInt(prompt('Digite o primeiro número: '));
+var n2 = parseInt(prompt('Digite o segundo número: '));
+
+var restoDaDivisão = n1 % n2;
+
+alert('O resto da divisão de '+n1+' Por '+n2+' é '+ restoDaDivisão);
+
+// --------------------------------------------------------------------
+
+//exercício 08 
+var palavraOuFrase = prompt('Digite uma palavra ou frase: ');
+
+alert('Essa texto contém '+palavraOuFrase.length+' caractéres');
+
+// --------------------------------------------------------------------
+
+//exercício 09
+var palavraOuFrase2 = prompt('Digite uma palavra ou frase: ');
+
+alert('A primeira letra deste texto é: '+ palavraOuFrase2[0].toUpperCase() +
+' e a ultima letra é : '+ palavraOuFrase2.slice(-1));
+
+// --------------------------------------------------------------------
+
+//exercício 10
+var nome = prompt('Digite o nome: ');
+var sobreNome = prompt('Digite o sobrenome: ');
+var idade = prompt('Digite a idade: ');
+var setor = prompt('Digite o setor: ');
 
 
-
+alert('Nome: '+nome[0].toUpperCase()+nome.substring(1)+' '+
+sobreNome[0].toUpperCase()+sobreNome.substring(1)+
+' - idade: '+idade+' - setor: '+setor[0].toUpperCase()+setor.substring(1));
 
