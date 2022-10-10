f9b469ffa84dbc5976657a39b6177d1e6bb06c26
Math2SystemsDev <math.systems.dev@gmail.com>
2022-10-09 22:46:36 -0300

    exercicio 01 resolvido

---
+++ script.js
@@ -0,0 +1,9 @@
+// exercício 01
+
+const valorLitroGasolina = 6.80;
+
+var valorDaCompra = prompt('Quanto deseja comprar de gasolina ? ');
+
+valorDaCompra = valorDaCompra / valorLitroGasolina;
+
+alert('o cliente abasteceu: ' + valorDaCompra + 'Litros');
