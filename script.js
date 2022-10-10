7a79a5f8c2e6e02ca4acd8fd1a6cfb0c6eafd9d0
Math2SystemsDev <math.systems.dev@gmail.com>
2022-10-09 23:55:52 -0300

    resolução exercicios 2 e 3

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 // exercício 01
-
+//entrada de dados
 const valorLitroGasolina = 6.80;
-
 var valorDaCompra = prompt('Quanto deseja comprar de gasolina ? ');
-
+//processamento
 valorDaCompra = valorDaCompra / valorLitroGasolina;
+//saída
+alert('O Cliente abasteceu: ' + valorDaCompra + ' Litros');
 
-alert('o cliente abasteceu: ' + valorDaCompra + 'Litros');
+// --------------------------------------------------------------------
+
+// exercício 02
+var temperaturaGC = parseInt(prompt('digite a temperatura: '));
+
+var Fahrenheit = ((9 * temperaturaGC) + 160) / 5;
+
+alert('O valor '+ temperaturaGC +' em Fahrenheit é '+ Fahrenheit);
+
+// --------------------------------------------------------------------
+
+//exercício 03
+var temperaturaFH = parseInt(prompt('Digite a temperatura: '));
+
+var grauCelsius = ((temperaturaFH - 32) * 5) / 9;
+
+alert('O valor '+ temperaturaFH + ' em Celsius é: '+ grauCelsius);
+
+//exercício 04
+
+
+
+
